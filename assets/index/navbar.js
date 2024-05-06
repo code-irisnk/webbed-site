3b53bc7e6ca2dc5264582ffb31097e0c191696ca
Iris De Santis <irisnk@elrant.team>
2024-05-06 20:43:33 +0200

    Prettier ! as in the npm package

--- assets/index/navbar.js
+++ assets/index/navbar.js
@@ -1,1 +1,3 @@
-console.log("If you're reading this, you're probably smart enough to know that this is a placeholder file, because I have no idea how the fuck to make a Navbar, let alone a responsive website that works on mobile. Again, PR me plz...")
+console.log(
+  "If you're reading this, you're probably smart enough to know that this is a placeholder file, because I have no idea how the fuck to make a Navbar, let alone a responsive website that works on mobile. Again, PR me plz...",
+);
