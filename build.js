91d498dbffa4f7d3728d76393744e8897e7aa3ee
Iris De Santis <irisnk@elrant.team>
2024-06-14 20:22:58 +0200

    as i thought, i am epic

--- build.js
+++ build.js
@@ -1,7 +1,11 @@
-const { execSync } = require('child_process');
+const {
+    execSync
+} = require('child_process');
 const fs = require('fs');
 
 const commitSha = execSync('git rev-parse --short HEAD').toString().trim();
 console.log(`Current commit SHA: ${commitSha}`);
 
-fs.writeFileSync('commit-sha', JSON.stringify({ sha: commitSha }));
+fs.writeFileSync('commit-sha', JSON.stringify({
+    sha: commitSha
+}));
