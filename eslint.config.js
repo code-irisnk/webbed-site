370ddc16fd96ec2acd79ae9fdace312319744b8c
Iris Nkrichronos <irisnk@elrant.team>
2025-11-19 15:22:08 +0100

    Smol consistency improvements

--- eslint.config.js
+++ eslint.config.js
@@ -43,7 +43,7 @@ export default tseslint.config(
         'error',
         {
           printWidth: 120,
-          htmlWhitespaceSensitivity: 'ignore',
+          htmlWhitespaceSensitivity: 'strict',
           bracketSameLine: true,
           singleAttributePerLine: false,
         },
