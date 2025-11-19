149d2603ff86ce5d3972095d273a7e643c5cc3df
Iris Nkrichronos <irisnk@elrant.team>
2025-11-19 11:20:10 +0100

    round

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,12 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { NavigationEnd, Router, RouterLink } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header-component',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [RouterLink, NgOptimizedImage],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   standalone: true,
 })
-export class HeaderComponent implements OnInit {
-  private router = inject(Router);
-
-  currentRoute = '';
-
-  ngOnInit() {
-    // Subscribe to router events to get the current route
-    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
-      // Extract the base route without parameters
-      this.currentRoute = event.urlAfterRedirects.split('/')[1] || '';
-    });
-  }
-
-  isCurrentRoute(route: string): boolean {
-    // Convert an empty route 'home' for comparison
-    const normalizedRoute = route === '' ? 'home' : route;
-    const normalizedCurrentRoute = this.currentRoute === '' ? 'home' : this.currentRoute;
-    return normalizedRoute === normalizedCurrentRoute;
-  }
-}
+export class HeaderComponent {}
