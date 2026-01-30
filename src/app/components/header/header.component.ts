94ea7374ea2cf0b1b354cece815ab668dbd6cc95
Iris Nkrichronos <irisnk@elrant.team>
2026-01-30 17:25:11 +0100

    hyuge changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
+import { IMAGE_LOADER, NgOptimizedImage } from '@angular/common';
+import { profileImageLoader } from './profilePicImageLoader';
 
 @Component({
   selector: 'app-header-component',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
+  providers: [
+    {
+      provide: IMAGE_LOADER,
+      useValue: profileImageLoader,
+    },
+  ],
   standalone: true,
 })
 export class HeaderComponent {}
