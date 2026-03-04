21dfacc299c732d2abae0ef161bafb2fe24b6bdd
Iris Nkrichronos <irisnk@elrant.team>
2026-03-04 15:03:46 +0100

    New blog entry, fix view transitions

--- src/app/pages/blog/blog-registry.ts
+++ src/app/pages/blog/blog-registry.ts
@@ -10,18 +10,18 @@ export interface BlogPostMeta {
 
 export const BLOG_POSTS: BlogPostMeta[] = [
   {
-    slug: 'hello-world',
-    title: 'Hello, World!',
-    date: 'March 3, 2026',
-    description: 'First post. You know how it goes.',
-    loadComponent: () => import('./posts/hello-world/hello-world.component').then((m) => m.HelloWorldPostComponent),
-  },
-  {
     slug: 'i-like-music-2025',
     title: 'I like Music (2025)',
     date: 'March 4, 2026',
     description: 'A recap of my favorite music of 2025',
     loadComponent: () =>
       import('./posts/i-like-music-2025/i-like-music-2025.component').then((m) => m.ILikeMusic2025PostComponent),
+  },
+  {
+    slug: 'hello-world',
+    title: 'Hello, World!',
+    date: 'March 3, 2026',
+    description: 'First post. You know how it goes.',
+    loadComponent: () => import('./posts/hello-world/hello-world.component').then((m) => m.HelloWorldPostComponent),
   },
 ];
---
+++ src/app/pages/blog/posts/i-like-music-2025/i-like-music-2025.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-post-i-like-music-2025',
+  templateUrl: './i-like-music-2025.component.html',
+  styleUrl: './i-like-music-2025.component.scss',
+  standalone: true,
+  imports: [RouterLink],
+})
+export class ILikeMusic2025PostComponent {}
