e35bb34747e0323813a7deb6e6859185f90b3752
Iris Nkrichronos <irisnk@elrant.team>
2026-05-31 12:58:33 +0200

    Add Atom and JSON feeds, with respective icons.

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BLOG_POSTS, BlogPostMeta } from './blog-registry';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
-  imports: [RouterLink],
+  imports: [RouterLink, MatIcon],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss',
 })
 export class BlogComponent {
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+
+    iconRegistry.addSvgIcon('my-rss', sanitizer.bypassSecurityTrustResourceUrl('icons/rss.svg'));
+    iconRegistry.addSvgIcon('my-atom', sanitizer.bypassSecurityTrustResourceUrl('icons/atom.svg'));
+    iconRegistry.addSvgIcon('my-jsonfeed', sanitizer.bypassSecurityTrustResourceUrl('icons/jsonfeed.svg'));
+  }
   readonly posts: BlogPostMeta[] = BLOG_POSTS;
 }
--- src/generate-feed.mjs
+++ src/generate-feed.mjs
@@ -3,9 +3,11 @@ import * as fs from 'fs';
 
 import { BLOG_POSTS } from './app/pages/blog/blog-registry.ts';
 
-const filename = 'src/app/assets/blog/feed.xml';
+const xmlFilename = 'src/app/assets/blog/feed.xml';
+const jsonFilename = 'src/app/assets/blog/feed.json';
 const today = new Date();
 
+// RSS feed
 const feed = new RSS({
   title: 'irisnk.me',
   description: 'A nerd talks about music, computers and stuff.',
@@ -42,4 +44,85 @@ for (const post of BLOG_POSTS) {
 }
 
 const xml = feed.xml();
-fs.writeFileSync(filename, xml);
+fs.writeFileSync(xmlFilename, xml);
+
+// JSON Feed
+function parseDate(value) {
+  const date = new Date(value);
+  return Number.isNaN(date.valueOf()) ? undefined : date.toISOString();
+}
+
+const jsonFeed = {
+  version: 'https://jsonfeed.org/version/1',
+  title: 'irisnk.me',
+  home_page_url: 'https://irisnk.me',
+  feed_url: 'https://irisnk.me/blog/feed.json',
+  description: 'A nerd talks about music, computers and stuff.',
+  user_comment: "JSON Feed for Iris Nkrichronos' blog",
+  icon: 'https://irisnk.me/favicon/android-chrome-192x192.png',
+  author: {
+    name: 'Iris Nkrichronos',
+    url: 'https://irisnk.me',
+    email: 'the@irisnk.me',
+  },
+  items: BLOG_POSTS.map((post) => {
+    const item = {
+      id: `https://irisnk.me/blog/${post.slug}`,
+      url: `https://irisnk.me/blog/${post.slug}`,
+      title: post.title,
+      summary: post.description,
+      content_text: post.description,
+      date_published: parseDate(post.date),
+    };
+
+    if (post.ogImage) {
+      item.image = post.ogImage;
+    }
+
+    return item;
+  }),
+  expired: false,
+  authors: [
+    {
+      name: 'Iris Nkrichronos',
+      url: 'https://irisnk.me',
+      email: 'the@irisnk.me',
+    },
+  ],
+  items_count: BLOG_POSTS.length,
+  last_updated: today.toISOString(),
+};
+
+fs.writeFileSync(jsonFilename, JSON.stringify(jsonFeed, null, 2));
+
+// Atom feed
+const atomFilename = 'src/app/assets/blog/atom.xml';
+const atomEntries = BLOG_POSTS.map((post) => {
+  const updated = parseDate(post.date) || today.toISOString();
+  const title = `<![CDATA[${post.title}]]>`;
+  const summary = `<![CDATA[${post.description}]]>`;
+  const enclosure = post.ogImage ? `<link rel="enclosure" type="image/jpeg" href="${post.ogImage}"/>` : '';
+  return [
+    '  <entry>',
+    `    <title>${title}</title>`,
+    `    <link href="https://irisnk.me/blog/${post.slug}"/>`,
+    `    <id>https://irisnk.me/blog/${post.slug}</id>`,
+    `    <updated>${updated}</updated>`,
+    `    <summary>${summary}</summary>`,
+    enclosure,
+    '  </entry>',
+  ].filter(Boolean).join('\n');
+}).join('\n');
+
+const atom = `<?xml version="1.0" encoding="utf-8"?>\n` +
+  `<feed xmlns="http://www.w3.org/2005/Atom">\n` +
+  `  <title>irisnk.me</title>\n` +
+  `  <link href="https://irisnk.me/blog/atom.xml" rel="self"/>\n` +
+  `  <link href="https://irisnk.me/"/>\n` +
+  `  <updated>${today.toISOString()}</updated>\n` +
+  `  <id>https://irisnk.me/</id>\n` +
+  `  <author><name>Iris Nkrichronos</name><email>the@irisnk.me</email></author>\n` +
+  `${atomEntries}\n` +
+  `</feed>`;
+
+fs.writeFileSync(atomFilename, atom);
